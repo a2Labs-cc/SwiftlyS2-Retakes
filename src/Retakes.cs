45eb4929be980e449665ada7f1c265bfb640d40e
shmitzas <46751666+shmitzas@users.noreply.github.com>
2026-04-08 21:59:18 +0300

    fix: Bump version to 1.2.0

--- src/Retakes.cs
+++ src/Retakes.cs
@@ -11,7 +11,7 @@ using SwiftlyS2_Retakes.Logging;
 
 namespace SwiftlyS2_Retakes;
 
-[PluginMetadata(Id = "Retakes", Version = "1.1.7", Name = "Retakes", Author = "aga", Description = "No description.")]
+[PluginMetadata(Id = "Retakes", Version = "1.2.0", Name = "Retakes", Author = "aga", Description = "No description.")]
 
 public partial class SwiftlyS2_Retakes : BasePlugin
 {
